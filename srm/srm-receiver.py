1245ab3c5ba50cb457f8d565355b4260fc1e873d
ibairos <ibai.ros@hotmail.com>
2019-05-01 19:33:26 +0200

    Version 1.0 of the SRM Stop&Wait protocol (Not tested in RPi yet)
    Still have to detect encoding and finish the HELLO protocol

---
+++ srm/srm-receiver.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Receiver part for the Short Range Mode competition of Team B
+# This version uses STOP&WAIT with timeout if the ACK is not received
+# It also uses CRC to ensure packet integrity
+# Author: Ibai Ros
+# Date: 05/01/2019
+# Version: 1.0
+
+import RPi.GPIO as GPIO
+
+from lib_nrf24 import NRF24
+import spidev
+import sys
+import time
+import crc16
+
+# Initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+# Define constants
+SENDER_CSN = 25
+SENDER_CE = 0
+SENDER_CHANNEL = 0x70  # Channel 60
+SENDER_PIPE = pipes[0]
+RECEIVER_CSN = 22
+RECEIVER_CE = 1
+RECEIVER_CHANNEL = 0x60  # Channel 70
+RECEIVER_PIPE = pipes[1]
+DATA_SIZE = 27  # 27 bytes
+DATA_TIMEOUT = 0.01  # 10 ms
+HELLO_TIMEOUT = 0.01  # 10 ms
+
+
+def initialize_radios(csn, ce, channel):
+    """ This function initializes the radios, each
+    radio being the NRF24 transceivers.
+
+    It gets 3 arguments, csn = Chip Select, ce = Chip Enable
+    and the channel that will be used to transmit or receive the data."""
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(csn, ce)
+    time.sleep(2)
+    radio.setRetries(15, 15)
+    radio.setPayloadSize(32)
+    radio.setChannel(channel)
+
+    radio.setDataRate(NRF24.BR_250KBPS)
+    radio.setPALevel(NRF24.PA_MIN)
+    radio.setAutoAck(False)
+    radio.enableDynamicPayloads()
+    radio.enableAckPayload()
+
+    return radio
+
+
+def wait_for_data(receiver):
+    """ This is a blocking function that waits
+    until the ACK is available in the receiver pipe
+    or until the timeout expires. """
+    start_time = time.time()
+    while not receiver.available(RECEIVER_PIPE):
+        if time.time() - start_time < DATA_TIMEOUT:
+            time.sleep(0.001)
+        else:
+            return False
+    return True
+
+
+def wait_for_hello(receiver):
+    """ This is a blocking function that waits
+    until we receive a HELLO message or the
+    timeout expires. """
+    start_time = time.time()
+    while not receiver.available(RECEIVER_PIPE):
+        if time.time() - start_time < HELLO_TIMEOUT:
+            time.sleep(0.001)
+        else:
+            return False
+    return True
+
+
+def send_packet(sender, payload):
+    """ Send the packet through the sender radio. """
+    sender.write(payload)
+
+
+def check_crc(crc, payload):
+    """ Function that checks the CRC and returns the result """
+    crc_str = str(bytes(crc))
+    if len(crc_str) < 5:
+        padding_length = 5 - len(crc_str)
+        if padding_length != 0:
+            for i in range(0, padding_length):
+                crc_str = '0' + crc_str
+    crc_payload = str(bytes(crc16.crc16xmodem(bytes(payload)).encode('utf-8')))
+    if crc_payload == crc_str:
+        return True
+    else:
+        return False
+
+
+def write_file(file_path, payload_list):
+    """ Function that stores the file in memory """
+    with open(file_path, "wb") as f:
+        for chunk in payload_list:
+            f.write(chunk)
+
+
+def hello(sender, receiver):
+    # Wait for the HELLO message until we get one
+    # TODO implement 3-way (SYN -> SYN + ACK -> ACK) handshake
+    hello_rcv = False
+    while not hello_rcv:
+        if wait_for_hello(receiver):
+            hello_syn = []
+            receiver.read(hello_syn, receiver.getDynamicPayloadSize())
+            if bytes(hello_syn) == b'HELLO':
+                send_packet(sender, b'HELLOACK')
+                hello_rcv = True
+                print("Received HELLO. Starting file reception...")
+
+
+def main():
+    """ This main function initializes the radios,
+    receives the file and stores it in memory. """
+
+    # Initialize the sender radio and print details
+    sender = initialize_radios(SENDER_CE, SENDER_CSN, SENDER_CHANNEL)
+    sender.openWritingPipe(SENDER_PIPE)
+    print("Sender Information")
+    sender.printDetails()
+
+    # Initialize the receiver radio and print details
+    receiver = initialize_radios(RECEIVER_CE, RECEIVER_CSN, RECEIVER_CHANNEL)
+    receiver.openReadingPipe(0, RECEIVER_PIPE)
+    print("Receiver Information")
+    receiver.printDetails()
+
+    # Initialize loop variables and functions
+    rx_success = False
+    seq_num = 1
+    payload_list = list()
+
+    # HELLO function, commented for now
+    # hello(sender, receiver)
+
+    # Recieve file
+    while not rx_success:
+        receiver.startListening()
+        rx_buffer = []
+        received_something = False
+        while not received_something:
+            if wait_for_data(receiver):
+                receiver.read(rx_buffer, receiver.getDynamicPayloadSize())
+                received_something = True
+            elif len(payload_list) != 0:
+                send_packet(sender, b'ACK')
+
+        receiver.stopListening()
+        if bytes(rx_buffer) != b"ENDOFTRANSMISSION":
+            crc = rx_buffer[:-5]
+            payload = rx_buffer[-5:]
+            if check_crc(crc, payload):
+                send_packet(sender, b'ACK')
+                payload_list.append(bytes(payload))
+                seq_num = seq_num + 1
+                print("Packet number " + seq_num + " received successfully")
+            else:
+                send_packet(sender, b'ERROR')
+                print("    Packet number " + seq_num + " received incorrectly")
+            receiver.startListening()
+        else:
+            send_packet(sender, b'ACK')
+            rx_success = True
+            print("RECEPTION SUCCESSFUL")
+
+    write_file(sys.argv[1], payload_list)
+
+
+if __name__ == '__main__':
+    main()
---
+++ srm/srm-sender.py
@@ -0,0 +1,220 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Receiver part for the Short Range Mode competition of Team B
+# This version uses STOP&WAIT with timeout if the ACK is not received
+# It also uses CRC to ensure packet integrity
+# Author: Ibai Ros
+# Date: 05/01/2019
+# Version: 1.0
+
+import RPi.GPIO as GPIO
+
+from lib_nrf24 import NRF24
+import spidev
+import sys
+import os
+import time
+import crc16
+
+# Initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+# Define constants
+SENDER_CSN = 25
+SENDER_CE = 0
+SENDER_CHANNEL = 0x60  # Channel 60
+SENDER_PIPE = pipes[1]
+RECEIVER_CSN = 22
+RECEIVER_CE = 1
+RECEIVER_CHANNEL = 0x70  # Channel 70
+RECEIVER_PIPE = pipes[0]
+DATA_SIZE = 27  # 27 bytes
+ACK_TIMEOUT = 0.05  # 50 ms
+HELLO_TIMEOUT = 0.01  # 10 ms
+
+
+def initialize_radios(csn, ce, channel):
+    """ This function initializes the radios, each
+    radio being the NRF24 transceivers.
+
+    It gets 3 arguments, csn = Chip Select, ce = Chip Enable
+    and the channel that will be used to transmit or receive the data."""
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(csn, ce)
+    time.sleep(2)
+    radio.setRetries(15, 15)
+    radio.setPayloadSize(32)
+    radio.setChannel(channel)
+
+    radio.setDataRate(NRF24.BR_250KBPS)
+    radio.setPALevel(NRF24.PA_MIN)
+    radio.setAutoAck(False)
+    radio.enableDynamicPayloads()
+    radio.enableAckPayload()
+
+    return radio
+
+
+def read_file(file_path):
+    """ Gets the provided file and reads its content as bytes,
+    after that, it stores everything in the variable payload_list,
+    which it returns. """
+
+    payload_list = list()
+
+    if os.path.isfile(file_path):
+        print("Loading File in: " + file_path)
+
+        with open(file_path, 'rb') as f:
+            while True:
+                chunk = f.read(DATA_SIZE)
+                if chunk:
+                    payload_list.append(chunk)
+                else:
+                    break
+    else:
+        print("ERROR: file does not exist in PATH: " + file_path)
+
+    print("Length of the file in chunks: " + str(len(payload_list)))
+
+    return payload_list
+
+
+def wait_for_ack(receiver):
+    """ This is a blocking function that waits
+    until the ACK is available in the receiver pipe
+    or until the timeout expires. """
+    start_time = time.time()
+    while not receiver.available(RECEIVER_PIPE):
+        if time.time() - start_time < ACK_TIMEOUT:
+            time.sleep(0.001)
+        else:
+            return False
+    return True
+
+
+def wait_for_hello(receiver):
+    """ This is a blocking function that waits
+    until we receive a HELLOACK message back or
+    the timeout expires. """
+    start_time = time.time()
+    while not receiver.available(RECEIVER_PIPE):
+        if time.time() - start_time < HELLO_TIMEOUT:
+            time.sleep(0.001)
+        else:
+            return False
+    return True
+
+
+def send_packet(sender, payload):
+    """ Send the packet through the sender radio. """
+    sender.write(payload)
+
+
+def calculate_crc(payload):
+    """ This is a function for calculating the crc
+    and making sure it has the right length. """
+    crc_str = str(crc16.crc16xmodem(payload))
+    padding_length = 5 - len(crc_str)
+    if padding_length != 0:
+        for i in range(0, padding_length):
+            crc_str = '0' + crc_str
+    return crc_str
+
+
+def build_frame(payload):
+    crc = bytes(calculate_crc(payload).encode('utf-16'))
+    return crc + payload
+
+
+def detect_encoding(payload_list):
+    # Detect encoding and return it
+    # TODO implement detect_encoding function
+    return None
+
+def hello(sender, receiver):
+    # Send HELLO messages until we get a response
+    # TODO implement 3-way (SYN -> SYN + ACK -> ACK) handshake
+    hello_rcv = False
+    while not hello_rcv:
+        send_packet(sender, b'HELLO')
+        if wait_for_hello(receiver):
+            hello_ack = []
+            receiver.read(hello_ack, receiver.getDynamicPayloadSize())
+            if bytes(hello_ack) == b'HELLOACK':
+                hello_rcv = True
+                print("Received HELLO ACK. Starting file transmission...")
+
+
+def main():
+    """ This main function initializes the radios and sends
+    all the data gathered from the file. """
+
+    # Initialize the sender radio and print details
+    sender = initialize_radios(SENDER_CE, SENDER_CSN, SENDER_CHANNEL)
+    sender.openWritingPipe(SENDER_PIPE)
+    print("Sender Information")
+    sender.printDetails()
+
+    # Initialize the receiver radio and print details
+    receiver = initialize_radios(RECEIVER_CE, RECEIVER_CSN, RECEIVER_CHANNEL)
+    receiver.openReadingPipe(0, RECEIVER_PIPE)
+    print("Receiver Information")
+    receiver.printDetails()
+
+    # Read file
+    payload_list = read_file(sys.argv[1])
+
+    # Initialize loop variables and functions
+    tx_success = False
+    seq_num = 1
+
+    # HELLO function, commented for now
+    # hello(sender, receiver)
+
+    # Send file
+    while not tx_success:
+        # Sending payload
+        for payload in payload_list:
+            retransmit = True
+            attempt = 0
+            while retransmit:
+                receiver.flush_rx()
+                send_packet(sender, build_frame(payload))
+                attempt = attempt + 1
+                ack = []
+                if wait_for_ack(receiver):
+                    receiver.read(ack, receiver.getDynamicPayloadSize())
+                    if bytes(ack) == b'ACK':
+                        retransmit = False
+                        print("Packet number " + seq_num + " transmitted successfully")
+                        seq_num = seq_num + 1
+                else:
+                    print("    Attempt " + attempt + " to retransmit packet number " + seq_num)
+                    if attempt > 1000:
+                        exit("Program ended after trying to retransmit for more than 1000 times")
+
+        retransmit_final = True
+        attempt_final = 0
+        while retransmit_final:
+            send_packet(sender, b'ENDOFTRANSMISSION')
+            attempt_final = attempt_final + 1
+            ack = []
+            if wait_for_ack(receiver):
+                receiver.read(ack, receiver.getDynamicPayloadSize())
+                if bytes(ack) == b'ACK':
+                    retransmit_final = False
+                    tx_success = True
+                    print("TRANSMISSION SUCCESSFUL")
+            else:
+                print("    Attempt " + attempt_final + " to retransmit FINAL packet")
+                if attempt_final > 1000:
+                    exit("Program ended after failing to transmit the EOT message")
+
+
+if __name__ == '__main__':
+    main()
